7c87894f5b0ac0ce3faa4157a8d0a509be18bee3
igurupreeth <igurupreeth@gmail.com>
2026-06-30 11:10:07 +0530

    string functions forward direction pritging

--- src/string_functions/$tring_FUNCT_001.java
+++ src/string_functions/$tring_FUNCT_001.java
@@ -3,8 +3,18 @@ package string_functions;
 public class $tring_FUNCT_001 {
 	public static void main(String[] args) {
 		String str="keep going great thing takes time";
+		
 		String []arr=str.split(" ");
+		
+		System.out.println("\nPrinting the array of words in reverse order\n");
 		for(int i=arr.length-1;i>=0;i--)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		
+		System.out.println("\nTo print in forward direction\n");
+		
+		for(int i=0;i < arr.length; i++)
 		{
 			System.out.print(arr[i]+" ");
 		}
